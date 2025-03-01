7b7fa0b2313330ba9418fe15e632be4b34cd80a4
ledozeur <git@github.com>
2025-03-01 17:56:36 +0300

    Stub and tests for add

--- Tdd/src/main/java/ru/netology/PhoneBook.java
+++ Tdd/src/main/java/ru/netology/PhoneBook.java
@@ -4,5 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PhoneBook {
+    private static PhoneBook INSTANCE;
+    private final Map<String, String> pBook = new HashMap<>();
+
+    public PhoneBook() {
+    }
+
+    public static PhoneBook getInstance() {
+        if (INSTANCE == null) {
+            synchronized (PhoneBook.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new PhoneBook();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public int add(String phone,String name) {
+
+        return 0;
+    }
 
 }
--- Tdd/src/test/java/PhoneBookTest.java
+++ Tdd/src/test/java/PhoneBookTest.java
@@ -2,5 +2,23 @@ import org.junit.jupiter.api.Test;
 import ru.netology.PhoneBook;
 
 public class PhoneBookTest {
+    PhoneBook phoneBook = new PhoneBook();
+
+
+    @Test //Тест на правильный номер
+    public void testAdd1() {
+        //arrange
+        int expected;
+        int actual = 2;
+        //act
+        phoneBook.add("+79315851646", "Oleg");
+        phoneBook.add("+79315851647", "Oleg");
+        expected = phoneBook.add("+79315851646", "Oleg");
+        //assert
+        if (actual == expected) {
+            System.out.println("Тест на правильный номер пройден");
+        } else
+            throw new AssertionError("Тест на правильный номер не пройден");
+    }
 
 }
